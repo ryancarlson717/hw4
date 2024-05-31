bd7f0bc90e1cd002130af595f03536edaa50e4a0
Ryan Carlson <ryan.carlson1@kellogg.northwestern.edu>
2024-05-31 19:35:04 +0000

    Added image functionality and production ready

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,11 +7,20 @@ class EntriesController < ApplicationController
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = params["user_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
